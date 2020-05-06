24c466a2ea6e5af15206ac69dab98ca071525d07
Anjali Verma <anjaliverma471@gmail.com>
2020-05-07 02:37:31 +0530

    Get and Post request with error handling in react using axios

---
+++ src/containers/Blog/Blog.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+
+import Post from '../../components/Post/Post';
+import FullPost from '../../components/FullPost/FullPost';
+import NewPost from '../../components/NewPost/NewPost';
+import './Blog.css';
+import axios from 'axios';
+
+class Blog extends Component {
+    state = {
+        posts: [],
+        selectedPostId: null,
+        error: false
+
+    }
+    componentDidMount(){
+        axios.get("https://jsonplaceholder.typicode.com/posts")
+        .then((response) => {
+            const posts = response.data.slice(0,4); 
+            const updatedPosts = posts.map((post) => {
+                return {
+                    ...post,
+                    author: 'Anjali'
+                }
+
+            })
+            this.setState({posts: updatedPosts})
+        })
+        .catch((error) => {
+            this.setState({error:true})
+        })
+        
+    }
+
+    postSelectedHandler = (id) => {
+       this.setState({selectedPostId:id})
+    }
+    render () {
+
+        let posts = <p style={{textAlign:`center`}}>Somwthing went wrong</p>
+        if(!this.state.error){
+            posts= this.state.posts.map((post,id) => {
+                return <Post
+                         key={id} 
+                         title={post.title} 
+                         author={post.author}
+                         clicked={this.postSelectedHandler.bind(this, post.id)}></Post>
+             })
+        } 
+        return (
+            <div>           
+               <section className="Posts">
+                    {posts}
+                </section>
+                <section>
+                    <FullPost id={this.state.selectedPostId}/>
+                </section>
+                <section>
+                    <NewPost />
+                </section>
+            </div>
+        );
+    }
+}
+
+export default Blog;
